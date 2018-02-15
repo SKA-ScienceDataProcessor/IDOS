2c0e9a6bf20edf651bfefa07064e68e01218c6a8
Chen Wu <chen.wu@icrar.org>
2018-02-15 15:52:04 +0800

    prepare for the stream pipeline

--- test/Bracewell/submit_athena_no_proxy_gpu_spead2_receiver.sh
+++ test/Bracewell/submit_athena_no_proxy_gpu_spead2_receiver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=6
+#SBATCH --nodes=7
 #SBATCH --time=01:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
@@ -16,9 +16,13 @@ APP_ROOT="/home/wu082/proj/IDOS/test/Bracewell"
 SID=$(date +"spead2_receiver_N"$1_"%Y-%m-%dT%H-%M-%S")
 LOG_DIR=$APP_ROOT"/logs/"$SID
 mkdir -p $LOG_DIR # to remove potential directory creation conflicts later
-GRAPH_DIR="/home/wu082/proj/IDOS/logical_graphs/Bracewell/spead2_receiver.json"
+#GRAPH_DIR="/home/wu082/proj/IDOS/logical_graphs/Bracewell/spead2_receiver.json"
+GRAPH_DIR="/home/wu082/proj/IDOS/logical_graphs/Bracewell/jacal-ingest.json"
 CLUSTER="Bracewell"
 source /flush1/tob020/venvs/jacal/bin/activate
-mpirun -np 6  python -m dlg.deploy.pawsey.start_dfms_cluster -l $LOG_DIR -L $GRAPH_DIR -d -c $CLUSTER -v 3 
+DLG_MON_HOST="sdp-dfms.ddns.net"
+DLG_MON_PORT="8081"
+export PYTHONPATH=/home/wu082/proj/jacal/apps/python
+mpirun -np 7  python -m dlg.deploy.pawsey.start_dfms_cluster -l $LOG_DIR -L $GRAPH_DIR -c $CLUSTER -v 3 -m $DLG_MON_HOST -o $DLG_MON_PORT
 
 #-R "receiver"
--- test/Bracewell/submit_athena_no_proxy_gpu_spead2_sender.sh
+++ test/Bracewell/submit_athena_no_proxy_gpu_spead2_sender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=6
+#SBATCH --nodes=7
 #SBATCH --time=01:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
@@ -17,7 +17,9 @@ SID=$(date +"spead2_sender_N"$1_"%Y-%m-%dT%H-%M-%S")
 LOG_DIR=$APP_ROOT"/logs/"$SID
 mkdir -p $LOG_DIR # to remove potential directory creation conflicts later
 GRAPH_DIR="/home/wu082/proj/IDOS/logical_graphs/Bracewell/spead2_sender.json"
+#GRAPH_DIR="/home/wu082/proj/IDOS/logical_graphs/Bracewell/jacal-ingest.json"
 CLUSTER="Bracewell"
 source /flush1/tob020/venvs/jacal/bin/activate
-mpirun -np 6  python -m dlg.deploy.pawsey.start_dfms_cluster -l $LOG_DIR -L $GRAPH_DIR -d -c $CLUSTER -v 3
-
+DLG_MON_HOST="sdp-dfms.ddns.net"
+DLG_MON_PORT="8081"
+mpirun -np 7  python -m dlg.deploy.pawsey.start_dfms_cluster -l $LOG_DIR -L $GRAPH_DIR -c $CLUSTER -v 3 -m $DLG_MON_HOST -o $DLG_MON_PORT
