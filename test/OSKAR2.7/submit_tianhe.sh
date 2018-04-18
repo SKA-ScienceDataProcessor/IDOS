790f691e30eee5040880d56dc81045a52c828e2c
Baoqiang Lao <lbq@shao.ac.cn>
2018-04-18 10:34:26 +0800

    add .gitignore

--- test/OSKAR2.7/submit_tianhe.sh
+++ test/OSKAR2.7/submit_tianhe.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yhrun -N 1 -n 1 -p gpu python /BIGDATA/ac_shao_tan_1/OSKAR/oskar_reference_simulations/gleam_imaging_test_mpi/run.py sim_gleam.json
-#echo $RUNLINE
-#$RUNLINE >> /BIGDATA/ac_shao_tan_1/OSKAR/oskar_reference_simulations/gleam_imaging_test/log
+yhrun -N 1 -n 1 -p gpu python /home/lbq/IDOS/test/OSKAR2.7/simulate.py --skymodel ./sky_model/sky_cen_A_1k.mod
