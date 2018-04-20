d9c2e326f6f2955bd9da5f9e4a36bad179a07770
Baoqiang Lao <lbq@shao.ac.cn>
2018-04-20 10:40:37 +0800

    correct facet id

--- test/OSKAR_CASA/data_reduction.py
+++ test/OSKAR_CASA/data_reduction.py
@@ -26,21 +26,6 @@ def casa_imaging(ms_file, Nfacet, NID):
     casa.run_script_from_file('/BIGDATA/ac_shao_tan_1/OSKAR/OSKAR_CASA/no_daliuge_new/image.py',timeout = 6000000)
 
 
-#class myThread (threading.Thread):
-#   def __init__(self, threadID, ms_file, Nfacet):
-#      threading.Thread.__init__(self)
-#      self.threadID = threadID
-#      self.ms_file = ms_file
-#      self.Nfacet = Nfacet
-#   def run(self):
-#      print "Starting " + self.ms_file
-#      # Get lock to synchronize threads
-#      #threadLock.acquire()
-#      casa_imaging(self.ms_file, self.Nfacet, self.threadID)
-#      # Free lock to release next thread
-#      #threadLock.release()
-#
-
 if __name__ == "__main__":
  
     parser = argparse.ArgumentParser(description='Simulation script.',
@@ -55,22 +40,13 @@ if __name__ == "__main__":
   
     ms_file = args.ms_file
 
+    t0=Thread(target=casa_imaging(ms_file, Nfacet, 0))
     t1=Thread(target=casa_imaging(ms_file, Nfacet, 1))
     t2=Thread(target=casa_imaging(ms_file, Nfacet, 2))
     t3=Thread(target=casa_imaging(ms_file, Nfacet, 3))
-    t4=Thread(target=casa_imaging(ms_file, Nfacet, 4))
 
+    t0.start()
     t1.start()
     t2.start()
-    t3.start()
-    t4.start() 
+    t3.start() 
    
-    #threadLock = threading.Lock()
-
-    # Create new threads and start
-#    for t in range(4): 
-#       thr = myThread(t+1, ms_file, Nfacet)
-#       thr.start()
-       # Wait for all threads to complete
-#       thr.join()
-#    print "Exiting Main Thread" 
