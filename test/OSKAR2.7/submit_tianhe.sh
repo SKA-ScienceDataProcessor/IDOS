245b758c856cba95dfe26fda0c9126bddf65bb50
Baoqiang Lao <lbq@shao.ac.cn>
2018-04-27 10:19:48 +0800

    update submit job

--- test/OSKAR2.7/submit_tianhe.sh
+++ test/OSKAR2.7/submit_tianhe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yhrun -N 1 -n 1 -p gpu python /BIGDATA/ac_shao_tan_1/OSKAR/IDOS-master/test/OSKAR2.7/simulate.py --skymodel ./sky_model/sky_cen_A_20k.mod
+yhrun -N 1 -n 1 -p gpu python /BIGDATA/ac_shao_tan_1/OSKAR/IDOS-master/test/OSKAR2.7/simulate.py --skymodel ./sky_model/sky_cen_A_20k.mod --inifile  ./conf/sim_cenA_aa4.ini 
