65dd7c5e4719c8b4655249d2a9e8a51af81850ae
dodsonicrar <richard.dodson@icrar.org>
2018-12-20 07:46:01 +0000

    tweeks before pushing

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import glob,os,sys
 import numpy as np
 
-def_par=[100.0,150.0,10.0,'spead/sender/EOR+Foreground/sky_eor_model_f*.osm']
+def_par=[0.0,1500.0,10.0,'spead/sender/EOR+Foreground/sky_eor_model_f*.osm']
 for n in range(1,len(sys.argv)):
     def_par[n-1]=sys.argv[n]
 
@@ -12,7 +12,7 @@ glob_list=def_par[3] ## model file list
 
 model_list=glob.glob(glob_list)
 model_list.sort()
-print 'Working from %d models'%(len(model_list))
+print '#Working from %d models'%(len(model_list))
 
 os.system('cp demo-root.ini demo.ini')
 
@@ -32,9 +32,10 @@ if (frq[0]>start_f):
 if (frq[-1]<end_f):
     end_f=frq[-1]
 
-print 'Freq Range (and step) %f %f %f MHz'%(start_f,end_f,delta_f)
+print '#Freq Range (and step) %f %f %f MHz'%(start_f,end_f,delta_f)
 
 start_freq=np.arange(start_f,end_f,step=delta_f)
+IdCount=0
 for start_f in start_freq:
     cmd=[]
     use_model=model_list[np.where(frq>start_f)[0][0]]
@@ -42,10 +43,13 @@ for start_f in start_freq:
     cmd.append('oskar_sim_interferometer --set demo.ini observation/num_channels 1')
     cmd.append('oskar_sim_interferometer --set demo.ini observation/frequency_inc_hz %d'%(delta_f*1e6))
     cmd.append('oskar_sim_interferometer --set demo.ini sky/oskar_sky_model/file '+use_model)
-    cmd.append('oskar_sim_interferometer --set demo.ini interferometer/ms_filename '+'demo_f'+str(start_f)+'.ms')
+    cmd.append('oskar_sim_interferometer --set demo.ini interferometer/ms_filename demo_f%08.4f.ms'%(start_f))
     cmd.append('oskar_sim_interferometer demo.ini')
+    print '%d "'%(IdCount),
     for line in cmd:
-      print line
+      print '%s;'%(line),
+    print '"'
+    IdCount=IdCount+1
 
 
 
